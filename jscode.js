ab59ae98444b3fe55ff756358c9d29d4a530070f
shaishavkumar <68222889+shaishavkumar@users.noreply.github.com>
2020-07-13 15:23:53 +0530

    Add files via upload

---
+++ jscode.js
@@ -0,0 +1,7 @@
+function newPicture(){
+	var picture = document.getElementById("image4").src = "port4.jpg";
+}
+
+function oldPicture(){
+	var picture = document.getElementById("image4").src = "port.jpg";
+}
